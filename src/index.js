4b8a0bcc5f032564116a84b459243bb974ba8a5f
Vlad Rybakou <rybakou@MacBook-Pro-Vlad.local>
2020-02-21 03:05:26 +0300

    feat: implement towel-sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if(!Array.isArray(matrix)   || !isNaN(matrix)) return [];
+    for(let i = 0; i < matrix.length ; i++){
+        if((i + 1) % 2 === 0){
+            console.log("LLlsdfsdfsdflsdflsdfl");
+            
+            matrix[i] = matrix[i].reverse();
+        }
+    }
+
+  return matrix.join(',').replace(/,/g, ' ').split(' ');
 }
